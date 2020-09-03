3b3ae86a5f101fe34bf789da7dc5a5834e191a72
sunrungeng <1589886434@qq.com>
2020-09-03 13:16:31 +0800

    简化代码 chaincode v2.2

--- src/main/java/cn/edu/bit/controller/AuthController.java
+++ src/main/java/cn/edu/bit/controller/AuthController.java
@@ -1,16 +1,42 @@
 package cn.edu.bit.controller;
 
 import cn.edu.bit.utils.Init;
-import org.hyperledger.fabric.gateway.*;
+import org.hyperledger.fabric.gateway.Contract;
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Network;
+import org.hyperledger.fabric.gateway.Wallet;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    public static Path walletPath;
+    public static Wallet wallet;
+    public static Path networkConfigPath;
+    public static Gateway.Builder builder;
+    public static Gateway gateway;
+    public static Network network;
+
+    @RequestMapping("/initCert")
+    public static void initCert() throws IOException {
+        // Load a file system based wallet for managing identities.
+        walletPath = Paths.get("wallet");
+        wallet = Wallet.createFileSystemWallet(walletPath);
+
+        // load a CCP
+        networkConfigPath = Paths.get("..", "..", "first-network", "connection-org1.yaml");
+        builder = Gateway.createBuilder();
+        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
+        gateway = builder.connect();
+        network = gateway.getNetwork("mychannel");
+    }
 
     static {
         System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
@@ -30,89 +56,110 @@ public class AuthController {
 
     @RequestMapping("/registerTop")
     public String registerTop(String ID, String MPK) throws Exception {
-
-//        Genson genson = new Genson();
-        Path walletPath = Paths.get("wallet");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-
-        Path networkConfigPath = Paths.get("..", "..", "first-network", "connection-org1.yaml");
-        Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
-
-        byte[] result;
-        try (Gateway gateway = builder.connect()) {
-            Network network = gateway.getNetwork("mychannel");
-            Contract contract = network.getContract("Authentication");
-            result = contract.submitTransaction("registerTop", ID, MPK);
-        }
-        return new String(result);
+        byte[] resultBytes;
+        Contract contract = network.getContract("Authentication");
+        resultBytes = contract.submitTransaction("registerTop", ID, MPK);
+        return new String(resultBytes);
     }
 
     @RequestMapping("/registerNotTop")
     public String registerNotTop(String IDi, String IDi_1) throws Exception {
-
-        // Load a file system based wallet for managing identities.
-        Path walletPath = Paths.get("wallet");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-
-        // load a CCP
-        Path networkConfigPath = Paths.get("..", "..", "first-network", "connection-org1.yaml");
-        Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
-
         byte[] resultBytes;
-        try (Gateway gateway = builder.connect()) {
-            Network network = gateway.getNetwork("mychannel");
-            Contract contract = network.getContract("Authentication");
-
-            resultBytes = contract.submitTransaction("registerNotTop", IDi, IDi_1);
-            return new String(resultBytes);
-        }
+        Contract contract = network.getContract("Authentication");
+        resultBytes = contract.submitTransaction("registerNotTop", IDi, IDi_1);
+        return new String(resultBytes);
     }
 
     @RequestMapping("/queryMPK")
     public String queryMPK(String ID) throws Exception {
         byte[] result;
-
-        // Load a file system based wallet for managing identities.
-        Path walletPath = Paths.get("wallet");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-
-        // load a CCP
-        Path networkConfigPath = Paths.get("..", "..", "first-network", "connection-org1.yaml");
-        Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
-
-        // create a gateway connection
-        try (Gateway gateway = builder.connect()) {
-            // get the network and contract
-            Network network = gateway.getNetwork("mychannel");
-            Contract contract = network.getContract("Authentication");
-            result = contract.evaluateTransaction("queryMPK", ID);
-        }
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("queryMPK", ID);
         return new String(result);
     }
 
-    @RequestMapping("/verifyAndUploadPara")
-    public String verifyAndUploadPara(String IDi, String IDi_1, String message, String signature) throws Exception {
+    @RequestMapping("/verifyAndUploadMPK")
+    public String verifyAndUploadMPK(String IDi, String IDi_1, String message, String signature) throws Exception {
         byte[] result;
-
-        // Load a file system based wallet for managing identities.
-        Path walletPath = Paths.get("wallet");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-        // load a CCP
-        Path networkConfigPath = Paths.get("..", "..", "first-network", "connection-org1.yaml");
-        Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
-        // create a gateway connection
-        try (Gateway gateway = builder.connect()) {
-            // get the network and contract
-            Network network = gateway.getNetwork("mychannel");
-            Contract contract = network.getContract("Authentication");
-            result = contract.submitTransaction("verifyAndUpload", IDi, IDi_1, message, signature);
-        }
+        Contract contract = network.getContract("Authentication");
+        result = contract.submitTransaction("verifyAndUpload", IDi, IDi_1, message, signature);
         return new String(result);
     }
+
+    @RequestMapping("/verifyAndRegisterNotTop")
+//    public String verifyAndRegisterNotTop(String IDi, String IDi_1, String message, String signature) throws Exception {
+    public String verifyAndRegisterNotTop(String domain, String identity, String type, String time, String signature) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.submitTransaction("verifyAndRegisterNotTop", domain, identity, type, time, signature);
+        return new String(result);
+    }
+
+    @RequestMapping("/updateMPK")
+    public String updateMPK(String ID, String MPK) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.submitTransaction("verifyAndUpload", ID, MPK);
+        return new String(result);
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateInfo(String info) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.submitTransaction("update", info);
+        return new String(result);
+    }
+
+    @RequestMapping("/queryInfo")
+    public String queryInfo(String ID) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("query", ID);
+        return new String(result);
+    }
+
+    @RequestMapping("/queryZone")
+    public String queryZone(String ID) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("queryZone", ID);
+        return new String(result);
+    }
+
+    @RequestMapping("/queryZoneMPK")
+    public String queryZoneMPK(String ID) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("queryZoneMPK", ID);
+        return new String(result);
+    }
+
+    @RequestMapping("/queryZoneInfo")
+    public String queryZoneInfo(String ID) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("queryZoneInfo", ID);
+        return new String(result);
+    }
+
+    @RequestMapping("/queryZoneMPKAndID")
+    public String queryZoneMPKAndID(String ID) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("queryZoneMPKAndID", ID);
+        return new String(result);
+    }
+
+    @RequestMapping("/verifyDevice")
+    public String verifyDevice(String accesserIdentity, String accessedDevice, String action, String time, String signature) throws Exception {
+        byte[] result;
+        Contract contract = network.getContract("Authentication");
+        result = contract.evaluateTransaction("verifyDevice", accesserIdentity, accessedDevice, action, time, signature);
+        return new String(result);
+    }
+
+//    submitTransaction上链 evaluateTransaction查询
 
 //    @RequestMapping("/insert")
 //    public String insert(AuthUser user) throws Exception {
--- src/test/java/t1.java
+++ src/test/java/t1.java
@@ -1,6 +1,8 @@
+import cn.edu.bit.utils.Sha256Util;
 import net.sf.json.JSONObject;
 
 import java.util.Date;
+
 
 public class t1 {
 
@@ -8,9 +10,37 @@ public class t1 {
 //        Date date = new Date();
 //        String addTime = date.toString();
 //        System.out.println(addTime);
+//        JSONObject object = new JSONObject();
+//        object.put("mpk", "0127b9d60de9d9e37a32c9d664ccca43c33bb0566d4392d1aade73197db288c4230687d7eeca6fa2b430970c9a76a741b7256b60a15ad99607dfb9cfeb812577690a9063fcee9bdcc66b0cc6569daf3d36538c92eed32e7aaaed69a2cc1da27194552b4dc9bac1fbb885a8b2aab6610695e8c575d97b072413b1119001b883a8ee");
+//        object.put("m1", "bbbbaaaaacccc");
+//        System.out.println(object.toString());
+//        Date date = new Date();
+//        System.out.println(date.getTime());
+//        sign2();
+//        genInfoJson();
+        System.out.println(new Date());
+    }
+
+    public static void sign2() {
         JSONObject object = new JSONObject();
-        object.put("mpk", "0127b9d60de9d9e37a32c9d664ccca43c33bb0566d4392d1aade73197db288c4230687d7eeca6fa2b430970c9a76a741b7256b60a15ad99607dfb9cfeb812577690a9063fcee9bdcc66b0cc6569daf3d36538c92eed32e7aaaed69a2cc1da27194552b4dc9bac1fbb885a8b2aab6610695e8c575d97b072413b1119001b883a8ee");
-        object.put("m1", "bbbbaaaaacccc");
+        object.put("accesserIdentity", "tsinghua.edu.cn");
+        object.put("accessedDevice", "watch.tsinghua.edu.cn");
+        object.put("time", "1599026889379");
+        object.put("action", "open");
+        String hashString = Sha256Util.getSHA256(object.toString());
+        System.out.println(hashString);
+    }
+
+    public static void genInfoJson() {
+        JSONObject object = new JSONObject();
+        object.put("belongto", "");
+        object.put("identity", "tsinghua.edu.cn");
+        object.put("zone", "edu.cn");
+        object.put("idCount", 0);
+        object.put("deviceCount", 0);
+        object.put("publicParameter", "0197d9144ba59f7f2f175fc4a7890406c4a5c5f90c235b632e849c7c3410fc1fe43ba31b914b366d0be77b5c3353ae61aa142cb5b033b3af23da0771b9c9c4fd699d5191167b7d71e31b7eff8cefe201d9f82d28b839564b09fa72e6acfe6f51b10cd578c737def1a7b62712a77b579350a6898165d45dba1b7c44ddfe06233646");
+        object.put("addTime", "1599029263981");
+//        String hashString = Sha256Util.getSHA256(object.toString());
         System.out.println(object.toString());
     }
 
